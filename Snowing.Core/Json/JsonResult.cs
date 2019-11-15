03667c0efb13fe4a061e3240518b575b10bb35da
tiny <liuweizzuie.com>
2019-11-15 10:33:15 +0800

    首次签入

---
+++ Snowing.Core/Json/JsonResult.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Snowing.Json
+{
+    [JsonObject]
+    public class JsonResult
+    {
+        [JsonProperty(PropertyName = "status", Order = 0)]
+        public int Status { get; set; }
+
+        [JsonProperty(PropertyName = "message", Order = 1)]
+        public string Message { get; set; }
+
+        public bool IsSuccess()
+        {
+            return this.Status == 0;
+        }
+
+        public JsonResult()
+        {
+            this.Status = 0;
+            this.Message = "OK";
+        }
+    }
+
+    [JsonObject]
+    public class JsonResult<T> : JsonResult
+    {
+        [JsonProperty(PropertyName = "data", Order = 2)]
+        public T Data { get; set; }
+
+        public JsonResult()
+        {
+            this.Data = default(T);
+            this.Message = "OK";
+        }
+    }
+}
